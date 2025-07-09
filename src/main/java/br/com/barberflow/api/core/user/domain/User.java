54bb7ffa44832829f547d3c53cf07b1362b77eb4
drezix <andre.t.agostinis@gmail.com>
2025-07-08 23:49:10 -0300

    chore: Fix User.java

--- src/main/java/br/com/barberflow/api/core/user/domain/User.java
+++ src/main/java/br/com/barberflow/api/core/user/domain/User.java
@@ -6,12 +6,12 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "User") // Define que esta classe é uma entidade JPA
-@Table(name = "users") // Define o nome da tabela no banco de dados
-@Getter // Lombok: gera todos os getters
-@NoArgsConstructor // Lombok: gera um construtor sem argumentos (obrigatório pelo JPA)
-@AllArgsConstructor // Lombok: gera um construtor com todos os argumentos
-@EqualsAndHashCode(of = "id") // Lombok: cria o equals e hashCode baseado apenas no 'id'
+@Entity(name = "User")
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id
